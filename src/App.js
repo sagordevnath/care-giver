3b8539ec99cbe4e7fda374950f7111df04743142
sagor devnath <ssagor64@gmail.com>
2022-04-16 22:44:00 +0600

    setup route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Blogs from './Pages/Home/Blogs/Blogs';
+import Home from './Pages/Home/Home/Home';
+import Services from './Pages/Home/Services/Services';
+import Login from './Pages/Login/Login/Login';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 
@@ -8,7 +13,11 @@ function App() {
     <div>
       <Header></Header>
       <Routes>
-
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/services' element={<Services></Services>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
       <Footer></Footer>
       
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <h1>Header</h1>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand href="#home">Care Giver</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">      
+    </Nav>
+    <Nav>
+      <Nav.Link as={Link} to="/">Home</Nav.Link>
+      <Nav.Link as={Link} to="/Services">Services</Nav.Link>
+      <Nav.Link as={Link} to="/Blogs">Blogs</Nav.Link>
+      <Nav.Link as={Link} to="/About">About</Nav.Link>
+      <Nav.Link as={Link} to="/Login">Login</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
         </div>
     );
 };
