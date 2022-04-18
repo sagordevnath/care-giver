154b68e57fd74e8b72c49ae2bae46fce28067f13
sagor devnath <ssagor64@gmail.com>
2022-04-18 19:56:53 +0600

    responsive fixed

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -4,18 +4,7 @@ import './Checkout.css'
 const Checkout = () => {
   return (
     <div>
-      <div class="container">
-        <div class="brand-logo"></div>
-        <div class="brand-title">TWITTER</div>
-        <div class="inputs">
-          <label>EMAIL</label>
-          <input type="email" placeholder="example@test.com" />
-          <label>PASSWORD</label>
-          <input type="password" placeholder="Min 6 charaters long" />
-          <button type="submit">LOGIN</button>
-        </div>
-        <a href="https://twitter.com/prathkum">MADE BY PRATHAM</a>
-      </div>
+      
     </div>
   );
 };
